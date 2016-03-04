0ad5dfba7262a79d47cdbe997b2b7a4139357ade
Evgeny Zhdanovskiy <zhdanovskiy@playform.com>
2016-03-04 17:58:53 +0300

    lcov -r

--- mydaemon/test/coverage.sh
+++ mydaemon/test/coverage.sh
@@ -3,11 +3,18 @@
 cmake ../cppCTestExample/
 make -j3
 
-# ./mydaemon/MyDaemon
+# echo -e '\n# lcov --zerocounters --directory . ************'
+lcov --zerocounters --directory .
 ctest
-gcov-4.8 ../cppCTestExample/mylib/mymath.cpp --object-file ./mylib/CMakeFiles/MyMath.dir/mymath.cpp.gcno
 
-lcov --capture --directory ./ --output-file coverage.info
+# echo -e '\n# lcov --capture --directory . --output-file coverage.info ************'
+lcov --capture --directory . --output-file coverage.info
+lcov -r coverage.info /usr/include/\* -o coverage.info
+lcov -r coverage.info 3rdparty/\* -o coverage.info
+
+# echo -e '\n# genhtml coverage.info --output-directory coverage.out ************'
 genhtml coverage.info --output-directory coverage.out
 
-google-chrome ./coverage.out/index.html
+google-chrome /home/zhdanovskiy/src/cppCTestExample-build/coverage.out/index.html
+
+# gcov-4.8 ../cppCTestExample/mylib/mymath.cpp --object-file ./mylib/CMakeFiles/MyMath.dir/mymath.cpp.gcno
